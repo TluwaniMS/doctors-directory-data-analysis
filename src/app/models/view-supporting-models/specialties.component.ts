049db3929204ee227bfffdcd558eea497824ecd2
tluwaniMS <tluwani2020@gmail.com>
2021-10-21 05:29:52 +0200

    adding-view-supporting-models

---
+++ src/app/models/view-supporting-models/specialties.component.ts
@@ -0,0 +1,9 @@
+export const specialisations = [
+  { specialisationName: 'Pediatrician', specialisationKey: 'PDTN' },
+  { specialisationName: 'Dematologist', specialisationKey: 'DMTG' },
+  { specialisationName: 'Cardiologist', specialisationKey: 'CDGT' },
+  { specialisationName: 'Urologist', specialisationKey: 'URLG' },
+  { specialisationName: 'Neurologist', specialisationKey: 'NRLG' },
+  { specialisationName: 'Psychiatrist', specialisationKey: 'PSCT' },
+  { specialisationName: 'Radiologist', specialisationKey: 'RDLG' },
+];
