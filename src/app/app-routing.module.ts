dc1dbbf216f35af92d940179009df0ae0eab4ed3
tluwaniMS <tluwani2020@gmail.com>
2021-10-25 06:47:32 +0200

    configuring-project-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HospitalAnalysisComponent } from 'src/app/components/hospital-analysis/hospital-analysis.component';
+import { MunicipalityAnalysisComponent } from 'src/app/components/municipality-analysis/municipality-analysis.component';
+import { SpecialtyAnalysisComponent } from 'src/app/components/specialty-analysis/specialty-analysis.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'hospital-stats-view/:hospital-key',
+    component: HospitalAnalysisComponent,
+  },
+  {
+    path: 'municipality-stats-view/:municipality-key',
+    component: MunicipalityAnalysisComponent,
+  },
+  {
+    path: 'specialty-stats-view/:specialty-key',
+    component: SpecialtyAnalysisComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
