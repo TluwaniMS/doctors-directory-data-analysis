b21b3f9854b6be9f2f9ade9c32b6ad83e8191f86
tluwaniMS <tluwani2020@gmail.com>
2021-10-27 02:52:51 +0200

    addding-view-supporting-model-title-constants

---
+++ src/app/models/operational-support-models/view-supporting-model-titles.component.ts
@@ -0,0 +1,5 @@
+export enum ViewSupportingModelTitles {
+  Municipalities = 'Municipalities',
+  Hospitals = 'Hospitals',
+  Specialisations = 'Specialisations',
+}
--- src/app/models/view-supporting-models/main-side-panel-menu-items.component.ts
+++ src/app/models/view-supporting-models/main-side-panel-menu-items.component.ts
@@ -1,20 +1,21 @@
 import { Hospitals } from './hospitals.component';
 import { Municipalities } from './municipalities.component';
 import { Specialisations } from './specialties.component';
+import { ViewSupportingModelTitles } from '../operational-support-models/view-supporting-model-titles.component';
 
 export const MenuItems: any[] = [
   {
-    title: 'Municipalities',
+    title: ViewSupportingModelTitles.Municipalities,
     expanded: true,
     children: Municipalities,
   },
   {
-    title: 'Hospitals',
+    title: ViewSupportingModelTitles.Hospitals,
     expanded: false,
     children: Hospitals,
   },
   {
-    title: 'Specialisations',
+    title: ViewSupportingModelTitles.Specialisations,
     expanded: false,
     children: Specialisations,
   },
