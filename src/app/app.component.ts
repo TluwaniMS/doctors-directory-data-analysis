483cc1f23298c2c51b8878accf24d1f0cc12320d
tluwaniMS <tluwani2020@gmail.com>
2021-10-27 02:03:32 +0200

    adding-service-to-access-clicked-menu-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NbSidebarService } from '@nebular/theme';
+import { NbMenuService } from '@nebular/theme';
 import { NbMenuItem } from '@nebular/theme';
 import { MenuItems } from 'src/app/models/view-supporting-models/main-side-panel-menu-items.component';
 
@@ -12,7 +13,11 @@ import { MenuItems } from 'src/app/models/view-supporting-models/main-side-panel
 export class AppComponent {
   MenuContent: NbMenuItem[] = MenuItems;
 
-  constructor(private sidebarService: NbSidebarService) {}
+  constructor(private sidebarService: NbSidebarService, menu: NbMenuService) {
+    menu.onItemClick().subscribe((item) => {
+      console.log(item);
+    });
+  }
 
   displaySideBar() {
     this.sidebarService.toggle(false, 'left');
