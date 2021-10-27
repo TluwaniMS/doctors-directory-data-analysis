aa3923dc22bb18ce3fc47bf1fced6cdf087c2987
tluwaniMS <tluwani2020@gmail.com>
2021-10-27 02:40:27 +0200

    accessing-clicked-item-properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,48 @@ import { MenuItems } from 'src/app/models/view-supporting-models/main-side-panel
 })
 export class AppComponent {
   MenuContent: NbMenuItem[] = MenuItems;
+  itemTitle: any | string;
+  selectedItem: any | string;
 
   constructor(private sidebarService: NbSidebarService, menu: NbMenuService) {
-    menu.onItemClick().subscribe((item) => {
-      console.log(item);
+    menu.onItemClick().subscribe((itemClicked) => {
+      this.itemTitle = itemClicked.item.parent?.title;
+      this.selectedItem = itemClicked.item;
+
+      this.checkItemTitleAndCallLinkedFunction(
+        this.itemTitle,
+        this.selectedItem
+      );
     });
   }
 
   displaySideBar() {
     this.sidebarService.toggle(false, 'left');
+  }
+
+  checkItemTitleAndCallLinkedFunction(itemTitle: string, selectedItem: any) {
+    switch (itemTitle) {
+      case 'Municipalities':
+        this.getMunicipalityStatsByKey(selectedItem.municipalityKey);
+        break;
+      case 'Hospitals':
+        this.getHospitalStatsByKey(selectedItem.hospitalKey);
+        break;
+      case 'Specialisations':
+        this.getSpecialtyStatsByKey(selectedItem.specialisationKey);
+        break;
+    }
+  }
+
+  getHospitalStatsByKey(hospitalKey: string) {
+    console.log(hospitalKey);
+  }
+
+  getMunicipalityStatsByKey(municipalityKey: string) {
+    console.log(municipalityKey);
+  }
+
+  getSpecialtyStatsByKey(specialtyKey: string) {
+    console.log(specialtyKey);
   }
 }
