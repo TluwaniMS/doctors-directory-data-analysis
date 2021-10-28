6c451c410c2711e92e6a3a26ea256296fe9b7b4a
tluwaniMS <tluwani2020@gmail.com>
2021-10-28 10:17:31 +0200

    adding-logic-to-get-main-directory-stats

--- src/app/components/main-directory-stats-analysis/main-directory-stats-analysis.component.ts
+++ src/app/components/main-directory-stats-analysis/main-directory-stats-analysis.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { MainDirectoryStatsEndPointService } from 'src/app/services/end-point-services/main-directory-end-point-service.service';
 
 @Component({
   selector: 'app-main-directory-stats-analysis',
   templateUrl: './main-directory-stats-analysis.component.html',
-  styleUrls: ['./main-directory-stats-analysis.component.css']
+  styleUrls: ['./main-directory-stats-analysis.component.css'],
 })
 export class MainDirectoryStatsAnalysisComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private mainDirectoryStatsEndPointService: MainDirectoryStatsEndPointService
+  ) {}
 
   ngOnInit(): void {
+    this.mainDirectoryStatsEndPointService
+      .getMainDirectoryStats()
+      .then((response) => {
+        console.log(response);
+      });
   }
-
 }
