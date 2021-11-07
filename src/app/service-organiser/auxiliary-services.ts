2300af88f5f4a2d712788fc654d75b2490531995
tluwaniMS <tluwani2020@gmail.com>
2021-11-07 21:32:42 +0200

    adding-hospital-auxilary-stats-service

--- src/app/service-organiser/auxiliary-services.ts
+++ src/app/service-organiser/auxiliary-services.ts
@@ -1,7 +1,9 @@
 import { MainDirectoryStatsAuxiliaryService } from 'src/app/services/auxillary-services/main-directory-stats-auxilary.service';
 import { MunicipalityStatsAuxiliaryService } from 'src/app/services/auxillary-services/municipality-stats-auxiliary.service';
+import { HospitalStatsAuxiliaryService } from 'src/app/services/auxillary-services/hospitals-stats-auxialry.service';
 
 export const AuxiliaryServices = [
   MainDirectoryStatsAuxiliaryService,
   MunicipalityStatsAuxiliaryService,
+  HospitalStatsAuxiliaryService,
 ];
