b1a7ebb68476c031c8afedbaab796e15be6ab777
tluwaniMS <tluwani2020@gmail.com>
2021-11-08 06:10:04 +0200

    adding-gender-count-doughnut-chart-data-service

--- src/app/services/auxillary-services/hospitals-stats-auxialry.service.ts
+++ src/app/services/auxillary-services/hospitals-stats-auxialry.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@angular/core';
+import { DoctorsProperties } from 'src/app/models/operational-support-models/doctors-properties';
 
 @Injectable()
-export class HospitalStatsAuxiliaryService {}
+export class HospitalStatsAuxiliaryService {
+  formatGenderCountDoughnutChartData(data: any[]) {
+    const preparedGenderCountChartData = [];
+
+    const femalesCount = data.filter(
+      (count) => count.gender === DoctorsProperties.Female
+    );
+
+    const malesCount = data.filter(
+      (count) => count.gender === DoctorsProperties.Male
+    );
+
+    const preparedFemalesCount = {
+      name: femalesCount[0].gender,
+      value: femalesCount[0].total,
+    };
+
+    const preparedMalesCount = {
+      name: malesCount[0].gender,
+      value: malesCount[0].total,
+    };
+
+    preparedGenderCountChartData.push(preparedFemalesCount, preparedMalesCount);
+
+    return preparedGenderCountChartData;
+  }
+}
