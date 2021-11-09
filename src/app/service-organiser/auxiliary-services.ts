53d1d85c0ec0f9e948112fb3bcbddc337febd739
tluwaniMS <tluwani2020@gmail.com>
2021-11-09 19:51:55 +0200

    adding-shared-auxiliary-stats-service

--- src/app/service-organiser/auxiliary-services.ts
+++ src/app/service-organiser/auxiliary-services.ts
@@ -1,9 +1,11 @@
 import { MainDirectoryStatsAuxiliaryService } from 'src/app/services/auxillary-services/main-directory-stats-auxilary.service';
 import { MunicipalityStatsAuxiliaryService } from 'src/app/services/auxillary-services/municipality-stats-auxiliary.service';
 import { HospitalStatsAuxiliaryService } from 'src/app/services/auxillary-services/hospitals-stats-auxialry.service';
+import { SharedStatsAuxiliaryService } from 'src/app/services/auxillary-services/shared-stats-auxiliary.service';
 
 export const AuxiliaryServices = [
   MainDirectoryStatsAuxiliaryService,
   MunicipalityStatsAuxiliaryService,
   HospitalStatsAuxiliaryService,
+  SharedStatsAuxiliaryService,
 ];
