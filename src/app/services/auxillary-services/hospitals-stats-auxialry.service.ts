e99c99e104b821195f3d7a28f01e77528e0ce59b
tluwaniMS <tluwani2020@gmail.com>
2021-11-09 07:53:00 +0200

    adding-auxiliary-service-for-preparing-specialty-count-grouped-by-gender-for-hospitals

--- src/app/services/auxillary-services/hospitals-stats-auxialry.service.ts
+++ src/app/services/auxillary-services/hospitals-stats-auxialry.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { DoctorsProperties } from 'src/app/models/operational-support-models/doctors-properties';
+import { Specialisations } from 'src/app/models/view-supporting-models/specialties.component';
 
 @Injectable()
 export class HospitalStatsAuxiliaryService {
@@ -27,5 +28,44 @@ export class HospitalStatsAuxiliaryService {
     preparedGenderCountChartData.push(preparedFemalesCount, preparedMalesCount);
 
     return preparedGenderCountChartData;
+  }
+
+  formatTotalSpecialisationCountGroupedByGender(data: any[]) {
+    const formattedSpecialisationCountGroupedByGender: any[] = [];
+
+    Specialisations.forEach((specialisation) => {
+      const linkedSpecialty: any = data.filter(
+        (count) => count.specialtyName === specialisation.title
+      );
+
+      const totalFemaleCount = linkedSpecialty.genderCount.filter(
+        (count: any) => count.gender === DoctorsProperties.Female
+      );
+
+      const totalMaleCount = linkedSpecialty.genderCount.filter(
+        (count: any) => count.gender === DoctorsProperties.Male
+      );
+
+      const totalFemaleCountObject = {
+        name: DoctorsProperties.Female,
+        value: totalFemaleCount[0].total,
+      };
+
+      const totalMaleCountObject = {
+        name: DoctorsProperties.Male,
+        value: totalMaleCount[0].total,
+      };
+
+      const formattedTotalGenderCountInSpecialty = {
+        name: specialisation.title,
+        series: [totalFemaleCountObject, totalMaleCountObject],
+      };
+
+      formattedSpecialisationCountGroupedByGender.push(
+        formattedTotalGenderCountInSpecialty
+      );
+    });
+
+    return formattedSpecialisationCountGroupedByGender;
   }
 }
