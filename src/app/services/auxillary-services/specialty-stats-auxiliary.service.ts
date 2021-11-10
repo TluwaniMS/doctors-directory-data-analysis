43b16a1ae60dc4ca63a0298b7c6ac8d306599242
tluwaniMS <tluwani2020@gmail.com>
2021-11-10 09:29:37 +0200

    adding-auxillary-service-to-format-specialty-count-in-hospital-grouped-by-gender

--- src/app/services/auxillary-services/specialty-stats-auxiliary.service.ts
+++ src/app/services/auxillary-services/specialty-stats-auxiliary.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Hospitals } from 'src/app/models/view-supporting-models/hospitals.component';
+import { DoctorsProperties } from 'src/app/models/operational-support-models/doctors-properties';
 
 @Injectable()
-export class SpecialtyStatsAuxiliaryService {}
+export class SpecialtyStatsAuxiliaryService {
+  formatSpecialtyCountInHospitalsGroupedByGenderGraphDisplayData(data: any[]) {
+    const formattedSpecialtyCountInHospitalsGroupedByGender: any[] = [];
+
+    Hospitals.forEach((model) => {
+      const linkedHospital = data.filter(
+        (hospital) => hospital.hospitalName === model.title
+      );
+
+      const totalMalesFormattedData = {
+        name: DoctorsProperties.Male,
+        value: linkedHospital[0].totalMaleDoctors,
+      };
+
+      const totalFemalesFormattedData = {
+        name: DoctorsProperties.Female,
+        value: linkedHospital[0].totalFemaleDoctors,
+      };
+
+      const formattedTotalDoctorsInMunicipaltiyGroupedByGender = {
+        name: linkedHospital[0].hospitalName,
+        series: [totalMalesFormattedData, totalFemalesFormattedData],
+      };
+
+      formattedSpecialtyCountInHospitalsGroupedByGender.push(
+        formattedTotalDoctorsInMunicipaltiyGroupedByGender
+      );
+    });
+
+    return formattedSpecialtyCountInHospitalsGroupedByGender;
+  }
+}
